d8f94eeec93d3407292aca35ab7859d959a185bf
Othi <mnpq.raven@gmail.com>
2026-09-23 01:55:02 +0700

    move to crane for better builds

--- flake.nix
+++ flake.nix
@@ -1,55 +1,73 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
+    crane.url = "github:ipetkov/crane";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs =
     {
       flake-utils,
-      naersk,
+      crane,
       nixpkgs,
-      rust-overlay,
       self,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = (import nixpkgs) {
-          inherit system;
-          overlays = [
-            (import rust-overlay)
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+
+        # Common arguments can be set here to avoid repeating them later
+        # Note: changes here will rebuild all dependency crates
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+
+          buildInputs = [
+            # Add additional build inputs here
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
           ];
         };
-        toolchain = pkgs.rust-bin.fromRustupToolchain (
-          (fromTOML (builtins.readFile ./rust-toolchain.toml)).toolchain // { "components" = [ ]; }
+
+        wallthi = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+            # Additional environment variables or build phases/hooks can be set
+            # here *without* rebuilding all dependency crates
+            # MY_CUSTOM_VAR = "some value";
+          }
         );
-        naersk' = pkgs.callPackage naersk {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-        package = naersk'.buildPackage {
-          src = ./.;
-        };
       in
       {
+        checks = {
+          inherit wallthi;
+        };
+
         # nix build and nix run
-        packages = {
-          default = package;
-          wallthi = self.packages.${system}.default;
+        packages.default = wallthi;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = wallthi;
         };
 
         # nix develop
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = with pkgs; [
+            rust-analyzer
           ];
         };
       }
